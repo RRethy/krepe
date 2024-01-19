e176424ff2c51cb4e39f3c8e67ba91986b933f63
Adam P. Regasz-Rethy <adam.regaszrethy@gmail.com>
2024-01-18 22:00:04 -0500

    Add sample pkg

--- pkg/pkg/pkg.go
+++ pkg/pkg/pkg.go
@@ -20,7 +20,7 @@ func NewPkgFromPath(pkgPath string) (*Pkg, error) {
 
 	fileInfo, err := os.Stat(pkgPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to stat pkg path `%s`: %w", pkgPath, err)
+		return nil, fmt.Errorf("failed to get info for pkg: %w", err)
 	}
 	if !fileInfo.IsDir() {
 		return nil, fmt.Errorf("pkg path is not a directory: %s", pkgPath)
@@ -29,14 +29,14 @@ func NewPkgFromPath(pkgPath string) (*Pkg, error) {
 	name := filepath.Base(pkgPath)
 	krepe, err := NewKrepeFromPath(filepath.Join(pkgPath, "krepe.yaml"))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create krepe in pkg path `%s`: %w", pkgPath, err)
 	}
 
 	var resources []*resource
 	for _, fileImport := range krepe.Imports.Files {
 		resource, err := newResourceFromPath(filepath.Join(pkgPath, fileImport))
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to create resource `%s` in pkg `%s`: %w", fileImport, pkgPath, err)
 		}
 		resources = append(resources, resource)
 	}
@@ -45,7 +45,7 @@ func NewPkgFromPath(pkgPath string) (*Pkg, error) {
 	for _, pkgImport := range krepe.Imports.Packages {
 		pkg, err := NewPkgFromPath(filepath.Join(pkgPath, pkgImport.Name()))
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to create pkg `%s` in pkg `%s`: %w", pkgImport.Name(), pkgPath, err)
 		}
 		packages = append(packages, pkg)
 	}
